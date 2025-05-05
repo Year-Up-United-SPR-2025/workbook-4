cd7dd85cf8bbc59ebd0143ea9c4d9b83d1c2ef53
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-05 12:20:51 -0400

    started the main methods inputs

--- HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -24,5 +24,36 @@ public class Employee {
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
     }
-    
+
+    public double getRegularHours() {
+        return Math.min(40, hoursWorked);
+    }
+
+    public double getOvertimeHours() {
+        return Math.max(0, hoursWorked - 40);
+    }
+
+    public double getTotalPay() {
+        return (getRegularHours() * payRate) + (getOvertimeHours() * payRate * 1.5);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
 }
--- HotelOperations/src/main/java/com/pluralsight/Main.java
+++ HotelOperations/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,20 @@
 package com.pluralsight;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Room Input
+        System.out.println("Enter thge number of beds in the room: \n");
+        int beds = scanner.nextInt();
+
+        System.out.println("Enter the price of the rooms: \n");
+        double price = scanner.nextDouble();
+
+        System.out.println();
+
     }
 }
