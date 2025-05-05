448ee724adb75b4bb10adb2010a4d685e3caa3bf
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-05 11:53:39 -0400

    created the code needed for room and started on Reservations code

--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,9 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public
 }
--- HotelOperations/src/main/java/com/pluralsight/Room.java
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
 public class Room {
+    public int getNumberOfBeads;
+    public double getPrice;
+    public String isOccupied;
+    public String isDirty;
+    public String isAvailable;
+
+    public Room(int getNumberOfBeads, double getPrice, String isOccupied, String isDirty, String isAvailable) {
+        this.getNumberOfBeads = getNumberOfBeads;
+        this.getPrice = getPrice;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getGetNumberOfBeads() {
+        return getNumberOfBeads;
+    }
+
+    public double getGetPrice() {
+        return getPrice;
+    }
+
+    public String getIsOccupied() {
+        return isOccupied;
+    }
+
+    public String getIsDirty() {
+        return isDirty;
+    }
+
+    public String getIsAvailable() {
+        return isAvailable;
+    }
 }
